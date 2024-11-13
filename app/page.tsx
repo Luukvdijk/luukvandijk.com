7ca0ef1fe05b56d1fabd8330a8e088232a634e91
Luuk van Dijk <luukvandijk2003@gmail.com>
2024-11-13 12:52:07 +0100

    comming soon

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="text-4xl">Comming soon</h1>
+    </div>
+  );
+}
