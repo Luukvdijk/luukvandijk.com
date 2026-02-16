865e71a61414bee6aa5be5ba4ba32167dca08d80
Luuk van Dijk <luukvandijk2003@gmail.com>
2026-02-16 14:42:59 +0100

    refactor: improve JsonLd component by extracting serialization logic

--- app/components/JsonLd.tsx
+++ app/components/JsonLd.tsx
@@ -1,9 +1,23 @@
-export default function JsonLd({ data, id }: { data: unknown; id?: string }) {
+type JsonLdProps = {
+  data: unknown;
+  id?: string;
+};
+
+function serializeJsonLd(data: unknown) {
+  return JSON.stringify(data)
+    .replace(/</g, "\\u003c")
+    .replace(/>/g, "\\u003e")
+    .replace(/&/g, "\\u0026")
+    .replace(/\u2028/g, "\\u2028")
+    .replace(/\u2029/g, "\\u2029");
+}
+
+export default function JsonLd({ data, id }: JsonLdProps) {
   return (
     <script
       id={id}
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+      dangerouslySetInnerHTML={{ __html: serializeJsonLd(data) }}
     />
   );
 }
